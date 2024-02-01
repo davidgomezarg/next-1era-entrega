b0be7d34e461601cf2dd884300e2d726bb0498e1
David Gomez <davidgomezarg@gmail.com>
2024-02-01 17:12:41 -0300

    Navbar OK. Pagina not-found.js OK

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,9 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <div>
+      <header>
         <Navbar/>
-      </div>
+      </header>
       <body className={inter.className}>{children}</body>
     </html>
   );
